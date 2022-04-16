d9daf7a6df4f0b8a97f14d2da65e2aa3ea1cea0c
Bryan alay <bryanalmi27@hotmail.com>
2022-04-16 16:55:49 -0500

    nuevo commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Mensaje from './Mensaje';
 
-function App() {
+const Numeroxd = () => {
+  return <h1>123456789</h1>
+}
+
+const Description = () => {
+  return <h1>Esta es la app de entrenamiento con React y node.js, 
+    <br/> por cierto, este es un componente xD</h1>
+}
+
+const  App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Mensaje color='red' message='Estamos trabajando'/>
+      <Mensaje color='green' message='en un curso '/>
+      <Mensaje color='yellow' message='de react'/>
+      <Description />
+      <Numeroxd />
     </div>
   );
 }
